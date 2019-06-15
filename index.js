35026956b41f2e56d05979183066a0fd98c14b50
Aaron Chu <aaronchu357@gmail.com>
2019-06-15 15:11:25 -0400

    Done.

--- index.js
+++ index.js
@@ -10,13 +10,71 @@
  */
 
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+	let eligibleDates = this.timeInEvents.map(function (e) {
+			return e.date
+	})
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+	let payable = eligibleDates.reduce(function (memo, d) {
+			return memo + wagesEarnedOnDate.call(this, d)
+	}.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+	return payable
+}
+
+const createEmployeeRecord = (arr) => {
+  return {
+    firstName: arr[0],
+		familyName: arr[1],
+		title: arr[2],
+		payPerHour: arr[3],
+		timeInEvents: [],
+		timeOutEvents: []
+	}
+}
+
+const createEmployees = (employees) => {
+  return employees.map(employee => createEmployeeRecord(employee))
+}
+
+const createTimeInEvent = function(datestamp){
+  let [date, hour] = datestamp.split(" ")
+  this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date: date
+  })
+  return this
+}
+
+const createTimeOutEvent = function(datestamp){
+  let [date, hour] = datestamp.split(" ")
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    date: date,
+    hour: parseInt(hour, 10)
+  })
+  return this
+}
+
+const hoursWorkedOnDate = function(datestamp){
+  let [date, hour] = datestamp.split(" ")
+  let inRecord = this.timeInEvents.find(record => record.date === date)
+  let outRecord = this.timeOutEvents.find(record => record.date === date)
+  return (outRecord.hour - inRecord.hour) / 100
+}
+
+const wagesEarnedOnDate = function(datestamp){
+  return hoursWorkedOnDate.call(this, datestamp) * this.payPerHour
+}
+
+const calculatePayroll = function(records){
+  return records.reduce(((accu, curr) => accu + allWagesFor.call(curr)), 0)
+}
+
+const createEmployeeRecords = function(records){
+  return records.map(record => createEmployeeRecord(record))
+}
+
+const findEmployeebyFirstName = function(employees, name){
+  return employees.find(employee => employee.firstName === name)
 }
